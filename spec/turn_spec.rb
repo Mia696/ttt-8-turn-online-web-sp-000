871dd03c49374960d104d5b6e4a131c502223630
Mia <tamiapaschall@gmail.com>
2019-10-23 04:43:47 +0000

    Automatically backed up by Learn

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -1,1 +1,9 @@
 
+  describe '#input_to_index' do	  describe '#input_to_index' do
+
+
+    it 'convervets a user_input to an integer' do	    it 'converts a user_input to an integer' do
+      user_input = "1"	      user_input = "1"
+
+
+      expect(input_to_index(user_input)).to be_a(Integer)	      expect(input_to_index(user_input)).to be_a(Integer)
